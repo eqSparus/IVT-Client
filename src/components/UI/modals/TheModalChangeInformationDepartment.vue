fe75ceef0392a0b89addebd0054641b7dedee660
eqSparus <sparus-1693@yandex.ru>
2023-02-19 14:22:12 +0600

    Обновление модальных окон

--- src/components/UI/modals/TheModalChangeInformationDepartment.vue
+++ src/components/UI/modals/TheModalChangeInformationDepartment.vue
@@ -1,13 +1,123 @@
 <template>
-$END$
+  <app-modal-window :is-show="isShow"
+                    title="Редактировать информацию о кафедре"
+                    @close="$emit('close')"
+                    :is-footer="true">
+
+    <div class="begin-edit-container">
+      <input type="text"
+             placeholder="Введите название кафедры"
+             class="field-standard"
+             v-model="information.title"
+             aria-label="Введите название кафедры"/>
+
+      <input type="text"
+             placeholder="Введите девиз кафедры"
+             class="field-standard mt-10"
+             v-model="information.slogan"
+             aria-label="Введите девиз кафедры"/>
+
+      <div class="edit-block mt-10">
+
+        <input type="phone"
+               placeholder="Введите телефон кафедры"
+               class="field-standard"
+               v-model="information.phone"
+               aria-label="Введите телефон кафедры"/>
+
+        <input type="email"
+               placeholder="Введите почту кафедры"
+               class="field-standard"
+               v-model="information.email"
+               aria-label="Введите почту кафедры"/>
+
+      </div>
+
+      <input type="text"
+             placeholder="Введите адрес кафедры"
+             class="field-standard mt-10"
+             v-model="information.address"
+             aria-label="Введите адрес кафедры"/>
+
+      <app-department-link-item v-for="link in information.links" :key="link.id"
+                                :link="link.name"
+                                :href="link.href"
+                                class="mt-10"
+                                @deleteLink="removeLink(link.id)"/>
+
+      <div class="edit-block mt-20">
+        <input type="button"
+               value="ДОБАВИТЬ ССЫЛКУ"
+               class="btn-standard"
+               @click="addLink"/>
+        <input type="button" value="ОБНОВИТЬ" class="btn-standard"/>
+      </div>
+    </div>
+
+  </app-modal-window>
 </template>
 
-<script>
-export default {
-name: "TheModalChangeInformationDepartment"
-}
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import AppModalWindow from '@/components/UI/AppModalWindow.vue';
+import AppDepartmentLinkItem from '@/components/UI/items/AppDepartmentLinkItem.vue';
+import { useStore } from 'vuex';
+import { InformationDepartment } from '@/api/model/ModelTypes';
+
+export default defineComponent({
+  name: 'TheModalChangeInformationDepartment',
+  components: {
+    AppModalWindow,
+    AppDepartmentLinkItem,
+  },
+  props: {
+    isShow: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ['close'],
+  setup() {
+    const store = useStore();
+
+    const addLink = () => {
+      store.commit('addDepartmentLink', {
+        id: Math.random()
+          .toString(),
+        href: '',
+        name: '',
+      });
+    };
+
+    const removeLink = (id: string) => {
+      store.commit('removeLink', id);
+    };
+
+    return {
+      addLink,
+      removeLink,
+      information: computed(() => store.getters.getInformationOfDepartment as InformationDepartment),
+    };
+  },
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.begin-edit-container {
+  display: flex;
+  flex-flow: column;
+  width: 50vw;
+
+  .edit-block {
+    display: flex;
+    flex-flow: row;
+    gap: 10px;
+
+    input {
+      flex: 1;
+    }
+  }
+}
 
 </style>
--- src/components/UI/modals/TheModalSettingAccount.vue
+++ src/components/UI/modals/TheModalSettingAccount.vue
@@ -1,13 +1,82 @@
 <template>
-$END$
+  <app-modal-window :is-show="isShow"
+                    title="Редактирование аккаунта"
+                    @close="$emit('close')"
+                    :is-footer="true">
+
+    <div class="account-setting-container">
+
+      <div class="setting-email">
+
+        <input type="email"
+               class="field-standard"
+               placeholder="Адрес эл. почты"
+               aria-label="Эл. почта аккаунта"/>
+
+        <input type="button"
+               class="btn-standard mt-20"
+               value="ОБНОВИТЬ АДРЕС">
+
+      </div>
+
+      <div class="setting-password">
+
+        <input type="email"
+               class="field-standard"
+               placeholder="Введите новый пароль"
+               aria-label="Введите новый пароль"/>
+
+        <input type="email"
+               class="field-standard mt-10"
+               placeholder="Повторите пароль"
+               aria-label="Повторите пароль"/>
+
+        <input type="button" class="btn-standard mt-20" value="ОБНОВИТЬ ПАРОЛЬ">
+
+      </div>
+
+    </div>
+
+  </app-modal-window>
 </template>
 
-<script>
-export default {
-name: "TheModalSettingAccount"
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppModalWindow from '@/components/UI/AppModalWindow.vue';
+
+export default defineComponent({
+  name: 'TheModalSettingAccount',
+  components: { AppModalWindow },
+  props: {
+    isShow: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ['close'],
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.account-setting-container {
+  display: flex;
+  flex-flow: row;
+  gap: 20px;
+  width: 40vw;
+  align-items: flex-end;
+
+  .setting-email {
+    width: 50%;
+    display: flex;
+    flex-flow: column;
+  }
+
+  .setting-password {
+    width: 50%;
+    display: flex;
+    flex-flow: column;
+  }
+}
 
 </style>
