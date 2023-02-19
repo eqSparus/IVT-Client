0a073e2b9b1177bac81d9ec7b987e5a72e61ea3d
eqSparus <sparus-1693@yandex.ru>
2023-02-19 14:20:00 +0600

    Обновление моделей

--- src/api/model/ModelTypes.ts
+++ src/api/model/ModelTypes.ts
@@ -1,42 +1,58 @@
+export type Link = {
+  id: string,
+  name: string,
+  href: string
+}
+
+export type InformationDepartment = {
+  id: string,
+  title: string,
+  slogan: string,
+  phone: string,
+  email: string,
+  address: string,
+  links: Array<Link>
+}
+
 export type InformationFaculty = {
-    id: string,
-    title: string,
-    description: string
+  id: string,
+  title: string,
+  description: string
 }
 
 export type InformationDirection = {
-    id: string,
-    title: string,
-    degree: string,
-    formTraining: string,
-    durationTraining: string
+  id: string,
+  title: string,
+  degree: string,
+  formTraining: string,
+  durationTraining: string
 }
 
 export type InformationForEntrantItem = {
-    id: string,
-    title: string,
-    descriptions: string[]
+  id: string,
+  title: string,
+  descriptions: string[]
 }
 
 export type InformationForEntrant = {
-    id: string,
-    title: string,
-    items: InformationForEntrantItem[]
+  id: string,
+  title: string,
+  items: InformationForEntrantItem[]
 }
 
 export type InfoAboutDepartment = {
-    id: string,
-    address: string,
-    phone: string,
-    email: string
+  id: string,
+  address: string,
+  phone: string,
+  email: string
 }
 
 export type Teacher = {
-    id: string,
-    image: string,
-    firstName: string,
-    middleName: string,
-    lastName: string,
-    post: string,
-    scientificDegree: string,
+  id: string,
+  image: string,
+  firstName: string,
+  middleName: string,
+  lastName: string,
+  post: string,
+  scientificDegree: string,
 }
